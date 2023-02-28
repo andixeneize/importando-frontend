ee886a939342341551496b312bae31c0e98b7004
marcosal <marcos90.alonso90@gmail.com>
2023-02-28 20:54:21 -0300

    armado inicial de pagin login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import {Container, Row, Col} from 'react-bootstrap';
 import { useForm, SubmitHandler } from "react-hook-form";
+import styles from '@styles/index.module.css';
 
 
 type Inputs = {
@@ -16,29 +17,74 @@ const Home: NextPage = () => {
   console.log(watch("example")) // watch input value by passing the name of it
 
 
-	return <Container fluid>
-    <Row>
-      <Col>
-        <p>Home</p>
-      </Col>
-    </Row>
-    <Row>
-      <Col>
-        { /* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {/* register your input into the hook by invoking the "register" function */}
-          <input defaultValue="test" {...register("example")} />
+// 	return <Container fluid>
+//     <Row>
+//       <Col>
+//         <p>Home</p>
+//       </Col>
+//     </Row>
+//     <Row>
+//       <Col>
+//         { /* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+//         <form onSubmit={handleSubmit(onSubmit)}>
+//           {/* register your input into the hook by invoking the "register" function */}
+//           <input defaultValue="test" {...register("example")} />
           
-          {/* include validation with required or other standard HTML validation rules */}
-          <input {...register("exampleRequired", { required: true })} />
-          {/* errors will return when field validation fails  */}
-          {errors.exampleRequired && <span>This field is required</span>}
+//           {/* include validation with required or other standard HTML validation rules */}
+//           <input {...register("exampleRequired", { required: true })} />
+//           {/* errors will return when field validation fails  */}
+//           {errors.exampleRequired && <span>This field is required</span>}
           
-          <input type="submit" />
-        </form>
-      </Col>
-    </Row>
-  </Container>
+//           <input type="submit" />
+//         </form>
+//       </Col>
+//     </Row>
+//   </Container>
+// }
+
+return <div className={styles.loginBox}>
+            <h1>Registrate</h1>
+            <form action="">
+                <label htmlFor="UserName">Nombre de usuario</label>
+                <input type="text" placeholder='Ingrese su usuario'/>
+
+                <label htmlFor="password">Contraseña</label>
+                <input type="password" placeholder='Ingrese su contraseña'/>
+
+                <div className={styles.remember}>Recordarme <input type="checkbox" name="recordarme" id="recordarme"/></div>
+                
+                <input type="submit"/>
+                
+                  <a href="">Olvide mi contraseña</a> <br></br>
+                  <a href="">Crear una nueva cuenta</a>  
+
+            </form>
+      </div>
+
+
+return <Container fluid>
+<Row>
+  <Col>
+    <p>Home</p>
+  </Col>
+</Row>
+<Row>
+  <Col>
+    { /* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input defaultValue="test" {...register("example")} />
+      
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register("exampleRequired", { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <span>This field is required</span>}
+      
+      <input type="submit" />
+    </form>
+  </Col>
+</Row>
+</Container>
 }
 
 export default Home
