fb99612a4ebe99322fb8351e55eca3c7ba27af0d
Andres Sanchez <ae.sanchez89@gmail.com>
2023-06-19 20:07:06 -0300

    Ajustes de campos y dimensiones para despacho

--- src/pages/despacho.tsx
+++ src/pages/despacho.tsx
@@ -6,6 +6,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 interface IFormInput {
+  cliente: String;
+  pwd: String;
   generarRemito: String;
   agenciaOrigen: String;
   claveExterna: String;
@@ -19,25 +21,60 @@ interface IFormInput {
   rut: String;
   valorCR: String;
   facturaCR: String;
+  telefonoDest: String
+  mailDest: String
 }
+
+/*
+pruebas despacho
+{
+  "cliente": "1414",
+  "pwd": "1414",
+  "generarRemito": "N",
+  "agenciaOrigen": "001",
+  "claveExterna": "TestTestTest",
+  "tipo": "C",
+  "producto": "2",
+  "bultos": 1,
+  "kilos": 1,
+  "destinatario": "aaaaaa",
+  "direccion": "bbbbbb",
+  "localidad": "maldonado",
+  "rut": "213456777",
+  "valorCR": 0.0,
+  "facturaCR": "1",
+  "telefonoDest": "11111111",
+  "mailDest": "fake@email.com"
+}
+*/
 
 const Despacho: NextPage = () => {
   const { register, handleSubmit } = useForm<IFormInput>();
-  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+  const onSubmit: SubmitHandler<IFormInput> = async (formData) => {
+    console.log('Despacho: Submit')
+    console.log(formData);
+  }
 
   return (
     <div className={styles.despachoBox}>
-      <h1>Despacho</h1> 
+      <h1>Despachar</h1> 
       <form onSubmit={handleSubmit(onSubmit)}>
         <Container>
           <Row>
             <Col> 
-                <label >Remito</label>
-                <input  {...register("generarRemito")} />
+                <label >Generar Remito</label>
+                <select  {...register("generarRemito")}>
+                  <option value="N">No</option>
+                  <option value="S">Si</option>
+                </select>            
             </Col>
             <Col>
-                <label >Agencia de origen</label>
-                <input  {...register("agenciaOrigen")} />
+              <label >Modo de pago</label>
+                <select  {...register("tipo")}>
+                  <option value="C">Crédito (Mercado Pago)</option>
+                  <option value="A">A cobrar en destino</option>
+                </select>
             </Col>
           </Row>
           <Row>
@@ -46,11 +83,8 @@ const Despacho: NextPage = () => {
                 <input  {...register("claveExterna")} />
             </Col>
             <Col>
-              <label >Tipo (Modo de pago)</label>
-                <select  {...register("tipo")}>
-                  <option value="C">Crédito (Mercado Pago)</option>
-                  <option value="A">A cobrar en destino</option>
-                </select>
+                <label >Agencia de origen</label>
+                <input  {...register("agenciaOrigen")} />
             </Col>
           </Row>
           <Row>
@@ -60,13 +94,13 @@ const Despacho: NextPage = () => {
             </Col>
             <Col>
                 <label >Bultos</label>
-                <input  {...register("bultos")} />
+                <input type="number" {...register("bultos")} />
             </Col>
           </Row>
           <Row>
             <Col> 
                 <label >Kilos</label>
-                <input  {...register("kilos")} />
+                <input type="number" {...register("kilos")} />
             </Col>
             <Col>
                 <label >Destinatario</label>
@@ -83,8 +117,33 @@ const Despacho: NextPage = () => {
                 <input  {...register("localidad")} />
             </Col>
           </Row>
+          <Row>
+            <Col>
+                <label >Telefono destinatario</label>
+                <input  {...register("telefonoDest")} />
+            </Col>
+            <Col>
+                <label >Email destinatario</label>
+                <input  {...register("mailDest")} />
+            </Col>  
+          </Row>
+          <Row>
+            <Col>
+                <label >Cliente</label>
+                <input  {...register("cliente")} />
+            </Col>
+            <Col>
+                <label >Password</label>
+                <input  {...register("pwd")} />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
                 <label >RUT</label>
                 <input  {...register("rut")} />
+            </Col>
+            <Col></Col>
+          </Row>
           <Row>
             <Col>
                 <input type="submit" value="Enviar"/> 
